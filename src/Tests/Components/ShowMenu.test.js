d4a599d65dbfdc2a5688f4cf552813eca8b6aae8
ColetteOrdz <pcoletteor@gmail.com>
2021-12-10 09:58:57 -0600

    Delete comments Co-authored-by: nandiaz nancy.090495@gmail.com

--- src/Tests/Components/ShowMenu.test.js
+++ src/Tests/Components/ShowMenu.test.js
@@ -17,21 +17,3 @@ test('Testeando Showmenu', () => {
 
   // expect(contentBtnsMenus).toBeInTheDocument()
 });
-
-// import React from 'react';
-// import '@testing-library/react';
-// import '@testing-library/jest-dom/extend-expect';
-// import { render, cleanup } from '@testing-library/jest-dom';
-// import ShowMenu from '../../Components/ShowMenu';
-
-// afterEach(cleanup);
-
-// test('Testeando Showmenu', () => {
-//   const mockAddClient = jest.fn();
-//   const component = render(<ShowMenu addOrder={mockAddClient} />);
-//   component.debug();
-//   console.log(mockAddClient);
-//   // const contentBtnsMenus = screen.getByRole('input')
-
-//   // expect(contentBtnsMenus).toBeInTheDocument()
-// });
